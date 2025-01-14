5eb7520bcd950094c35121c58b09556ded17f214
mirajhad <mirajhadish48@gmail.com>
2025-01-14 11:24:53 +0530

    requirements.txt file updated

--- scrap.py
+++ scrap.py
@@ -1,2 +1,8 @@
-import os
-import  sklearn
+from selenium import webdriver
+
+website = 'https://www.flipkart.com/'
+path='C:/Users/miraj/OneDrive/Desktop/chromedriver-win64/chromedriver.exe'
+driver = webdriver.Chrome(path)
+driver.get(website)
+
+#driver.quit()
